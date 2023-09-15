203d454ceb29f9b4b5a5e40a0394a883434df7d1
Anish Awasthi <128889867+anish3333@users.noreply.github.com>
2023-09-15 19:34:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myNav");
-  if (x.className === "navbar") {
-    x.className += "responsive";
-  } else {
-    x.className = "navbar";
-  }
